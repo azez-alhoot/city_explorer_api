a4c4ef598600f5e1fba0a60830668e5dc3c89e58
azez-alhoot <azezalhoot82@yahoo.com>
2020-07-11 15:49:32 +0300

    lab9 done

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const cors = require('cors');
 
 const superagent = require('superagent');
 
+const yelp = require('yelp-fusion');
+const client = yelp.client(process.env.YELP_API_KEY);
+
 const pg = require('pg');
+const { move } = require('superagent');
+const { response } = require('express');
 
 
 const server = express();
@@ -25,7 +30,7 @@ function Location(city, locData) {
     this.latitude = locData[0].lat;
     this.longitude = locData[0].lon;
 }
-// http://localhost:4000/Location?city=amman
+// http://localhost:4000/location?city=amman
 server.get('/location', (req, res) => {
     let city = req.query.city;
     const key = process.env.PRIVATE_TOKEN;
@@ -41,17 +46,17 @@ server.get('/location', (req, res) => {
                 // console.log(result.rows[0].location_latitude, result.rows[0].location_longitude);
             } else {
                 superagent.get(url)
-                .then(data => {
-                    // console.log(data.body);
-                    const location = new Location(city, data.body);
-                    let safeValues = [city, location.formatted_query, location.latitude, location.longitude];
-                    const insert_values = `INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES($1,$2,$3,$4)`
-                    clint.query(insert_values, safeValues)
-                    .then(result => {
-                        console.log(result);
-                        res.status(200).json(location);
+                    .then(data => {
+                        // console.log(data.body);
+                        const location = new Location(city, data.body);
+                        let safeValues = [city, location.formatted_query, location.latitude, location.longitude];
+                        const insert_values = `INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES($1,$2,$3,$4)`
+                        clint.query(insert_values, safeValues)
+                            .then(result => {
+                                console.log(result);
+                                res.status(200).json(location);
+                            })
                     });
-                });
             }
         });
 });
@@ -69,7 +74,7 @@ server.get('/weather', (req, res) => {
             let weatherArr = data.body.data.map((value, idx) => {
                 const weather = new Weather(value);
                 return weather;
-            });
+            })
             res.status(200).json(weatherArr);
         });
 });
@@ -93,8 +98,8 @@ server.get('/trails', (req, res) => {
             let hikingArr = data.body.trails.map((value, idx) => {
                 const hike = new Hiking(value);
                 return hike;
-            });
-            console.log(hikingArr);
+            })
+            // console.log(hikingArr);
             // console.log(hike);
             res.status(200).json(hikingArr);
         });
@@ -114,7 +119,65 @@ function Hiking(value) {
     this.condition_time = value.conditionDate.split(' ', 2).slice(1).toString();
 }
 
+server.get(`/movies`, (req, res) => {
+    const city = req.query.search_query;
+    let key = process.env.MOVIE_API_KEY;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${city}&language=en-US`;
+
+    superagent.get(url)
+        .then(data => {
+            let newMovie = data.body.results.map((value, idx) => {
+                const movie = new Movies(value);
+                return movie;
+            })
+            console.log(newMovie);
+            res.status(200).json(newMovie);
+        });
+
+});
+
+
+function Movies(value) {
+    this.title = value.title;
+    this.overview = value.overview;
+    this.average_votes = value.vote_average;
+    this.total_votes = value.vote_count;
+    this.image_url = `https://image.tmdb.org/t/p/w500${value.poster_path}`;
+    this.popularity = value.popularity;
+    this.released_on = value.release_date;
+}
+
+function Resturants(value) {
+    this.name = value.name;
+    this.image_url = value.image_url;
+    this.price = value.price;
+    this.rating = value.rating;
+    this.url = value.url;
+}
+
+server.get(`/yelp`, (req, res) => {
+    let city = req.query.search_query;
+    const searchRequest = {
+        term: 'Food',
+        location: city
+    };
+    client.search(searchRequest)
+        .then(data => {
+            let resArr = data.jsonBody.businesses.map((vlue, idx) => {
+                let newRes = new Resturants(vlue);
+                return newRes;
+            })
+            console.log(resArr);
+            res.status(200).json(data.jsonBody.businesses);
+        })
+        .catch(e => {
+            console.log(e);
+        });
+});
+
+
 server.get('/', (req, res) => {
+
     res.status(200).send('you did great job');
 });
 
@@ -129,5 +192,5 @@ clint.connect()
     .then(() => {
         server.listen(PORT, () => {
             console.log(`listning on PORT ${PORT}`);
-        });
+        })
     });
